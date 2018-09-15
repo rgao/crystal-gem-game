198757f343003ec8d545494745a5097db010d6cc
rgao <xinghoshi@gmail.com>
2018-09-15 13:36:35 -0700

    html buttons and most jq code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,25 @@
-goal = Math.floor(Math.random);
+var goal_arr = [];
 
-var counter = 0
+for (var i = 0; i < 101; i++) {
+    goal_arr.push(i + 19);
+};
+
+goal = goal_arr[Math.floor(Math.random)];
+
+// var gem_value = {
+//     "ruby" : 4,
+//     "sapphire" : 6,
+//     "emerald" : 5,
+//     "diamond" : 7
+// }
+
+var score = 0
+
+$("button").on("click", function() {
+    score += $(this).data-gemvalue();
+    if (score === goal) {
+        // display win
+    } else if (score > goal) {
+        // display loss
+    };
+})
